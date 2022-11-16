fc2174c13aa54da839721a3fa0eb342fbb191ce0
abdulsagheer <abdulsagheeras29@gmail.com>
2022-11-16 10:45:47 +0530

    Initialized Parking-Management-System

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App" role="main">
-      <article className="App-article">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </article>
+      <h1>Parking-Management-System</h1>
     </div>
   );
 }
